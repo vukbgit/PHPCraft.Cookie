a16da94f0f65e88eaf766c259d4ac2caa0fcb227
vukbgit <info@vuk.bg.it>
2016-04-23 17:20:48 +0200

    added code comments

--- src/CookieBuilderInterface.php
+++ src/CookieBuilderInterface.php
@@ -2,9 +2,33 @@
 
 namespace PHPCraft\Cookie;
 
+/**
+ * Manages cookies
+ *
+ * @author vuk <info@vuk.bg.it>
+ */
 interface CookieBuilderInterface
 {
+    /**
+     * Sets cookie
+     *
+     * @param string $name
+     * @param mixed $value
+     **/
     public function set($name, $value);
     
-    public function get($name);
+    /**
+     * Gets cookie
+     *
+     * @param string $name
+     * @param mixed $defaultValue to return in case cookie is not set
+     **/
+    public function get($name, $defaultValue = false);
+    
+    /**
+     * Deletes cookie
+     *
+     * @param string $name
+     **/
+    public function delete($name);
 }
--- src/CookieBuilderPatrickLouysHttp.php
+++ src/CookieBuilderPatrickLouysHttp.php
@@ -4,12 +4,24 @@ namespace PHPCraft\Cookie;
 
 use Http\Request, Http\Response, Http\CookieBuilder;
 
+/**
+ * Manages cookies using Http class by Patrick Louys (https://github.com/PatrickLouys/http)
+ *
+ * @author vuk <info@vuk.bg.it>
+ */
 class CookieBuilderPatrickLouysHttp implements CookieBuilderInterface
 {
     private $cookieBuilder;
     private $httpRequest;
     private $httpResponse;
 
+    /**
+     * Constructor.
+     *
+     * @param Http\Request $httpRequest
+     * @param Http\Response $httpResponse
+     * @param Http\CookieBuilder $cookieBuilder
+     **/
     public function __construct(Request $httpRequest, Response $httpResponse, CookieBuilder $cookieBuilder)
     {
         $this->cookieBuilder = $cookieBuilder;
@@ -17,14 +29,37 @@ class CookieBuilderPatrickLouysHttp implements CookieBuilderInterface
         $this->httpResponse = $httpResponse;
     }
 
+    /**
+     * Sets cookie
+     *
+     * @param string $name
+     * @param mixed $value
+     **/
     public function set($name, $value)
     {
         $cookie = $this->cookieBuilder->build($name, $value);
         $this->httpResponse->addCookie($cookie);
     }
     
+    /**
+     * Gets cookie
+     *
+     * @param string $name
+     * @param mixed $defaultValue to return in case cookie is not set
+     **/
     public function get($name, $defaultValue = false)
     {
         $this->httpRequest->getCookie($name, $defaultValue);
+    }
+    
+    /**
+     * Deletes cookie
+     *
+     * @param string $name
+     **/
+    public function delete($name)
+    {
+        $cookie = $this->cookieBuilder->build($name);
+        $this->httpResponse->deleteCookie($cookie);
     }
 }
