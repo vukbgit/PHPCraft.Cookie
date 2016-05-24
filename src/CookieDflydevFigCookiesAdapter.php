c3ba60afe57b05f65f812ee5ae4feb4b3e848cb9
vukbgit <info@vuk.bg.it>
2016-05-24 17:39:37 +0200

    added CookieDflydevFigCookiesAdapter class file

---
+++ src/CookieDflydevFigCookiesAdapter.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace PHPCraft\Cookie;
+
+use Psr\Http\Message\Request, Dflydev\FigCookies\FigResponseCookies, Dflydev\FigCookies\FigRequestCookies;
+
+/**
+ * Manages cookies using Http class by Patrick Louys (https://github.com/PatrickLouys/http)
+ *
+ * @author vuk <info@vuk.bg.it>
+ */
+class CookieDflydevFigCookiesAdapter implements CookieInterface
+{
+    private $cookie;
+    private $httpRequest;
+    private $httpResponse;
+
+    /**
+     * Constructor.
+     *
+     * @param Http\Request $httpRequest
+     * @param Http\Response $httpResponse
+     * @param Http\CookieBuilder $cookie
+     **/
+    public function __construct(Request $httpRequest, Response $httpResponse, CookieBuilder $cookie)
+    {
+        $this->cookie = $cookie;
+        $this->cookie->setDefaultSecure(false);
+        $this->httpRequest = $httpRequest;
+        $this->httpResponse = $httpResponse;
+    }
+
+    /**
+     * Sets cookie
+     *
+     * @param string $name
+     * @param mixed $value
+     * @param integer $life of the cookie in seconds
+     **/
+    public function set($name, $value, $life = null)
+    {
+        FigResponseCookies::set($this->httpResponse, SetCookie::create($name)
+            ->withValue($value)
+            ->withExpires($life)
+        );
+    }
+    
+    /**
+     * Gets cookie
+     *
+     * @param string $name
+     * @param mixed $defaultValue to return in case cookie is not set
+     **/
+    public function get($name, $defaultValue = false)
+    {
+        return $cookie = FigRequestCookies::get($this->httpRequest, $name, $defaultValue);
+    }
+    
+    /**
+     * Deletes cookie
+     *
+     * @param string $name
+     **/
+    public function delete($name)
+    {
+        FigResponseCookies::remove($this->httpResponse, $name);
+    }
+}
